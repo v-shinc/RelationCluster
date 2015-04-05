9be848c1c2fd6bba424cdbf24519d5aec6f9fd20
Shini Chen <chensn456@163.com>
2015-04-05 16:33:51 +0800

    Revert "nothing"
    This reverts commit be6e532d91b5c8fd71b23dd23c43f923f5a2b63d.

--- relationCluster2.py
+++ relationCluster2.py
@@ -3,11 +3,14 @@ __author__ = 'chensn'
 import os
 import numpy as np
 import sktensor
-from numpy import sqrt
+
 import sklearn.cluster as skclus
 import argparse
 import time
 
+def supplement(params):
+        assert isinstance(params,dict)
+        return lambda k,v: params[k] if k in params else v
 class TensorDecompAdapter:
      @classmethod
      def cp_als(cls,dt,rank,**params):
@@ -23,7 +26,7 @@ class TensorCluster:
 
     @property
     def tensor3(self):
-        return self._t3
+        return self.__t3
 
     @tensor3.setter
     def tensor3(self,t3):
@@ -32,7 +35,7 @@ class TensorCluster:
         ndim = len(t3.shape)
         if ndim != 3:
             raise ValueError('Expected 3-order tensor, got {}-order'.format(ndim))
-        self._t3 = t3
+        self.__t3 = t3
 
     def __init__(self,n_cluster,tensor3,rels,dec,clu,decparams,cluparams,result_dir):
 
@@ -48,22 +51,24 @@ class TensorCluster:
         self.result_dir = result_dir
 
 
-    _decompo_param = {}
+    __decompo_param = {}
 
     def decompo_param(self,**params):
-        params['max_iter'] = params.get('max_iter',100)
-        self._decompo_param = params
+        default = supplement(params)
+        params['max_iter'] = default('max_iter',100)
+        self.__decompo_param = params
 
 
-    _cluster_param = {}
+    __cluster_param = {}
 
     def cluster_param(self,**params):
-        params['n_jobs'] = params.get('n_jobs',-2)
-        params['max_iter'] = params.get('max_iter',100)
+        default = supplement(params)
+        params['n_jobs'] = default('n_jobs',-2)
+        params['max_iter'] = default('max_iter',100)
         params['n_clusters'] = self.n_cluster
-        self._cluster_param = params
+        self.__cluster_param = params
 
-    def decompose(self,params):
+    def decompostion(self,params):
         t0 = time.time()
         print 'start tensor decomposing'
         self.rank = params.pop('rank',10)
@@ -83,13 +88,7 @@ class TensorCluster:
         t0 = time.time()
         print 'start clustering'
         sz = t3.shape
-        t3 = t3.reshape(sz[0]*sz[1],sz[2])
-        # Normalize
-        scalar = 1 # matrix is sparse
-        norm = sqrt((t3**2).sum(axis=0))/scalar
-        norm[norm < 1] = 1
-        t3 = (t3 / norm).T
-
+        t3 = t3.reshape(sz[0]*sz[1],sz[2]).T
         cluster = getattr(skclus,self.clu_method)(**params)
         cluster.fit(t3)
         print 'It costs {:0.2f} s to cluster'.format((time.time()-t0))
@@ -110,15 +109,12 @@ class TensorCluster:
         for i in range(self.n_cluster):
             with open(os.path.join(result_dir,'c'+str(i)),'w') as fid:
                 for w in clusters[i]:
-                    fid.write(w)
+                    fid.write(w+"\n")
         print "It costs {:0.2f} s to save results ".format((time.time()-t0))
 
     def save_tc_result(self,t3):
         path = os.path.join(self.result_dir,'tc{}'.format(self.rank))
-        try:
-            np.save(path,t3)
-        except IOError:
-            print "faild to save decomposed tensor"
+        np.save(path,t3)
 
     @property
     def quick(self):
@@ -129,12 +125,11 @@ class TensorCluster:
             self.quick_run()
             return
         print "start running"
-
-        P,fit,itr,exectimes = self.decompose(self._decompo_param)
+        P,fit,itr,exectimes = self.decompostion(self.__decompo_param)
         print "fit = {}\n itr = {}\n execute time = {} s\n".format(fit,itr,exectimes)
         if save_tc_res:
             self.save_tc_result(P)
-        result = self.cluster(P,self._cluster_param)
+        result = self.cluster(P,self.__cluster_param)
         self.save_cluster_result(result)
 
     '''
@@ -142,7 +137,7 @@ class TensorCluster:
     '''
     def quick_run(self):
         print "start running"
-        result = self.cluster(self.tensor3,self._cluster_param)
+        result = self.cluster(self.tensor3,self.__cluster_param)
         self.save_cluster_result(result)
 
 
@@ -154,15 +149,21 @@ if __name__ == "__main__":
     args = parse.parse_args()
     t3 = np.load(args.input)
     with open(args.rels) as fid:
-        rels = fid.readlines()  # ends with '\n'
+        rels = [r for r in fid]
 
-        for r in rels:
-            print r.strip()
-    tc1 = TensorCluster(60,t3,rels,
+    tc1 = TensorCluster(50,t3,rels,
                         TensorCluster.dm['cp_als'],
                         TensorCluster.cm['kmeans'],
-                        dict(rank=5,init='random'),
+                        dict(rank=10,init='random'),
                         dict(precompute_distances=True,verbose=0),
-                        'cp5_kmeans60')
+                        'cp10_kmeans50')
     result = tc1.run(save_tc_res=True)
 
+    # tc2 = TensorCluster(50,t3,rels,
+    #                     TensorCluster.dm['none'],
+    #                     TensorCluster.cm['kmeans'],
+    #                     dict(rank=10,init='random'),
+    #                     dict(precompute_distances=True,verbose=0),
+    #                     'cp10_kmeans50')
+    #
+    # tc2.run()
